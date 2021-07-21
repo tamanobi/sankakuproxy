12e8ac179a8801023a08c30557d98d1e7474f754
tamanobi <tamanobi@gmail.com>
2021-07-22 04:06:40 +0900

    feat: use cache

--- prox/gateways.py
+++ prox/gateways.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import requests
 
 
@@ -10,8 +12,14 @@ class SankakuAccessError(Exception):
 
 
 def request_sankaku():
+    tmp = Path(__file__).parent / Path("tmp")
+    if not tmp.exists():
+        tmp.mkdir()
+
     try:
         res = requests.get("https://chan.sankakucomplex.com")
+        with open(tmp / "index.html", "w") as f:
+            f.write(res.text)
         res.raise_for_status()
     except requests.exceptions.HTTPError:  # noqa
         raise SankakuAccessError
